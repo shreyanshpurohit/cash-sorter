3d2f9f310cf7428d75829104c92a5c4870699aea
Shreyansh Purohit <138966500+shreyanshpurohit@users.noreply.github.com>
2026-06-17 22:13:52 +0530

    Add files via upload

---
+++ cash_sorter.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+
+int main(void)
+{
+    int cents;
+
+    do 
+    {
+        printf("Change owed: ");
+        scanf("%d", &cents);
+    }
+    while (cents < 0);
+    
+    int quarters = calculate_quarters(cents);
+    cents -= quarters * 25;
+
+    int dimes = calculate_dimes(cents);
+    cents -= dimes * 10;
+
+    int nickels = calculate_nickels(cents);
+    cents -= nickels * 5;
+
+    int pennies = cents;
+
+    printf("Quarters: %d\n", quarters);
+    printf("Dimes: %d\n", dimes);
+    printf("Nickels: %d\n", nickels);
+    printf("Pennies: %d\n", pennies);
+    printf("Total coins: %d\n", quarters + dimes + nickels + pennies);
+
+    return 0;
+
+}
+
+int calculate_quarters(int cents)
+{
+    int quarters = 0;
+    while (cents >= 25)
+    {
+        quarters++;
+        cents -= 25;
+    }
+    return quarters;
+}
+
+int calculate_dimes(int cents)
+{
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes++;
+        cents -= 10;
+    }
+    return dimes;
+}
+
+int calculate_nickels(int cents)
+{
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        nickels++;
+        cents -= 5;
+    }
+    return nickels;
+}
